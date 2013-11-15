7541fd7ade53380d2d70c6f72c474b4fcedef5a0
sambodanis <sam.bodanis@gmail.com>
2013-11-15 09:26:45 +0000

    Server.js formatting fixed

--- index.js
+++ index.js
@@ -1,77 +1,75 @@
-(function() {
-    var Strategy, app, express, fs, passport, path, port, stuffDict;
+var Strategy, app, express, fs, passport, path, port, stuffDict;
 
-    express = require('express');
-    passport = require('passport');
-    Strategy = require('passport-local').Strategy;
-    path = require('path');
-    fs = require('fs');
+express = require('express');
+passport = require('passport');
+Strategy = require('passport-local').Strategy;
+path = require('path');
+fs = require('fs');
 
-    // Init express application
-    app = express();
-    app.use(express.logger());
-    app.use(express.bodyParser());
+// Init express application
+app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
 
-    stuffDict = {};
+stuffDict = {};
 
-    // User login config
-    passport.use(new Strategy(
-        function (username, password, done) {
-            // TODO: Auth method
-            return console.log(username, password, done);
+// User login config
+passport.use(new Strategy(
+    function (username, password, done) {
+        // TODO: Auth method
+        return console.log(username, password, done);
+    }
+));
+
+// TEMP Hello world
+app.get('/', function(req, res) {
+    return res.send('Hello World!\n');
+});
+
+// ...?
+app.get('/:key/:value', function(req, res) {
+    var k, v;
+    stuffDict[req.params.key] = req.params.value;
+    return res.send(((function() {
+        var _results;
+        _results = [];
+        for (k in stuffDict) {
+            v = stuffDict[k];
+            _results.push(k + " -> " + v + "\n");
         }
-    ));
+        return _results;
+    })()).join(""));
+});
 
-    // TEMP Hello world
-    app.get('/', function(req, res) {
-        return res.send('Hello World!\n');
-    });
+// Login callback - user auth
+app.post('/login',
+    passport.authenticate('local',
+    function (req, res) {
+        // Called on success
+        // e.g: res.redirect('/users/' + req.user.username);
+    }
+));
 
-    // ...?
-    app.get('/:key/:value', function(req, res) {
-        var k, v;
-        stuffDict[req.params.key] = req.params.value;
-        return res.send(((function() {
-            var _results;
-            _results = [];
-            for (k in stuffDict) {
-                v = stuffDict[k];
-                _results.push(k + " -> " + v + "\n");
-            }
-            return _results;
-        })()).join(""));
-    });
+// Root callback - show req
+app.post('/', function (req, res) {
+    return console.log(req);
+});
 
-    // Login callback - user auth
-    app.post('/login',
-        passport.authenticate('local',
-        function (req, res) {
-            // Called on success
-            // e.g: res.redirect('/users/' + req.user.username);
-        }
-    ));
+// File request callback
+app.post('/file', function(req, res) {
+    console.log('here1');
+    if (req.files) {
+        console.log('File exists');
+    } else {
+        console.log('File does not exist');
+    }
+    return res.send("Some image thing recieved\n");
+});
 
-    // Root callback - show req
-    app.post('/', function (req, res) {
-        return console.log(req);
-    });
+// Start listening
+port = process.env.PORT || 5000;
 
-    // File request callback
-    app.post('/file', function(req, res) {
-        console.log('here1');
-        if (req.files) {
-            console.log('File exists');
-        } else {
-            console.log('File does not exist');
-        }
-        return res.send("Some image thing recieved\n");
-    });
+app.listen(port, function() {
+    return console.log("Listening on " + port);
+});
 
-    // Start listening
-    port = process.env.PORT || 5000;
-
-    app.listen(port, function() {
-        return console.log("Listening on " + port);
-    });
-
-}).call(this);
