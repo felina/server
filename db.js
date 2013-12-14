7c85e8a85bc49a7c077b8c96a2b0d0d50bb6aa17
George Field <gf12815@my.bristol.ac.uk>
2013-12-14 22:37:41 +0000

    Add user functions, fix get hash.

--- db.js
+++ db.js
@@ -5,14 +5,49 @@ var conn = mysql.createConnection(dbCFG);
 
 function init() {
 	conn.connect(function(err) {
-		console.log(err.code);
-		console.log(err.fatal);
+		if (err) {
+			console.log(err.code);
+				console.log(err.fatal);
+		} else {
+			console.log("DB connected.");
+		}
 		});
 }
 
+// Adds a new user to users/local auth. TODO: Use a user object.
+function addNewUser(email, privilege, auth) {
+	var query = "INSERT INTO `users` VALUE (null,?,?)"
+	var sub = [email, privilege];
+	query = mysql.format(query, sub);
+	conn.query(query, function(err, res) {
+		if (err) {
+			console.log(err.code);
+			return false;
+		} else {
+			return setUserHash(res.insertId, auth);
+		}
+	});
+}
+
+function setUserHash(id, auth) {
+	var query = "INSERT INTO `local_auth` VALUE (?,?)";
+	var sub = [id, auth];
+	query = mysql.format(query, sub);
+	conn.query(query, function(err, res) {
+		if (err) {
+			// The query failed, respond to the error.
+			console.log(err.code);
+			return false;
+		} else {
+			console.log(res);
+			return true;
+		}
+	});
+}
+
 // Looks up a users bcrypt hash from their registered email	
-var getUserHash = function (email) {
-	var query = "SELECT `local_auth`.`hash` FROM `local_auth` INNER JOIN `users` ON `user_id` WHERE `users`.`email` == ??";
+function getUserHash(email) {
+	var query = "SELECT `hash` FROM `local_auth` INNER JOIN `users` USING (`userid`) WHERE `email` = ?";
 	var sub = [email];
 	query = mysql.format(query, sub);
 	conn.query(query, function(err, res) {
@@ -28,3 +63,4 @@ var getUserHash = function (email) {
 
 module.exports.init = init;
 module.exports.getUserHash = getUserHash;
+module.exports.addNewUser = addNewUser;
