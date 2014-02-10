6aa0fdd640740db88c4d4f5dfcfe279c30f44396
George Field <gf12815@my.bristol.ac.uk>
2014-02-10 13:42:20 -0500

    Refactor dummy job endpoints into jobs.js. (#23)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var md5 = require('MD5');
 var aws = require('aws-sdk');
 var db = require('./db.js');
 var images = require('./images.js');
+var jobs = require('./jobs.js');
 
 // Setup passport
 auth.authSetup(passport);
@@ -136,61 +137,8 @@ app.get('/img/:id/meta', function(req, res) {
     }
 });
 
-// Job start req
-app.post('/start', function (req, res) {
-    // Get the image IDs for processing
-    var idData = req.files;
-    var images = [];
-    for (var imageName in idData) {
-        images.push(idData[imageName]);
-    }
-    if (images.length > 0) {
-        return res.send('Some image IDs received');
-    } else {
-        return res.send('Need to specify images for job');
-    }
-
-    /*if (req.files) {
-        console.log('File exists');
-        // console.log(req.files);
-        console.log('Num files: ' + Object.keys(req.files).length)
-
-    } else {
-        console.log('File does not exist');
-    }
-    return res.send("Some image thing recieved\n");*/
-});
-
-// Job progress check
-app.get('/progress', function (req, res) {
-    var jobID = req.get('jobID');
-    if (jobID) {
-        console.log('Job progress req: jobID ' + jobID);
-        // TODO: Query job server
-        var progress = 0.74;
-        return res.send({ 'progress': progress });
-    }
-    else {
-        return res.send('No jobID provided');
-    }
-});
-
-// Job results
-app.get('/results', function (req, res) {
-    var jobID = req.get('jobID');
-    if (jobID) {
-        console.log('Job results req: jobID ' + jobID);
-        // TODO: Query job server
-        return res.send({ 'data': [{ 'some': 'data' }, { 'some more': 'data' }] });
-    }
-    else {
-        return res.send('No jobID provided');
-    }
-});
-
-app.post('/target', function (req, res) {
-    console.log('posted executable to target')
-});
+// Import job related routes, mostly dummy endpoints for now
+jobs.jobRoutes(app);
 
 // Start listening
 port = process.env.PORT || 5000;
