e83c174bc3be8476c32c843ffdbbad88fbaf6e9d
George Field <gf12815@my.bristol.ac.uk>
2014-03-05 13:55:59 -0500

    Add cleaner constructor for error responses.

--- error.js
+++ error.js
@@ -1,5 +1,12 @@
+function APIErrResp(code, msg) {
+    this.res = false;
+    this.err = new APIError(code, msg);
+}
+
 // Convenience function to create an error to send to the client.
-module.exports.APIError = function(code, msg) {
+function APIError(code, msg) {
     this.code = code;
     this.msg = msg;
 }
+
+module.exports = {APIErrResp:APIErrResp, APIError:APIError};
