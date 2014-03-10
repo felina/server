c1a8cee6c4983a602dc9fd40899dba308a415201
sambodanis <sam.bodanis@gmail.com>
2014-03-10 00:36:51 +0000

    auto deploy test commit§1f

--- checkNew.py
+++ checkNew.py
@@ -7,24 +7,34 @@ import os
 #
 while True:
     try:
-        print "Checking for commits at " + strftime("%Y-%m-%dT%H:%M:%S")
-        #urlContents = urllib2.urlopen('https://api.github.com/repos/felina/server/commits?since=' + timeStore)
-        urlContents = urllib2.urlopen('https://api.github.com/repos/felina/server/commits')
+        a = os.popen('git stash && git pull').read()
+        #print a
+        if 'Already up-to-date.' not in a:
+            print 'New stuff!'
     except Exception, e:
-        pass
+        raise e
     else:
-        try:
-            print "Loading URL contents"
-            result = json.load(urlContents)
-        except Exception, e:
-            pass
-        else:
-            currHash = os.popen('git rev-parse HEAD').read()[:-1]
-            if currHash is not result[0]['sha']:
-                # new pull stuff
-                print 'New commits! ' + result[0]['sha'] + ' vs curr ' + currHash
-                os.system('git stash && git pull')
-                pass
+        pass
+    # try:
+    #     print "Checking for commits at " + strftime("%Y-%m-%dT%H:%M:%S")
+    #     #urlContents = urllib2.urlopen('https://api.github.com/repos/felina/server/commits?since=' + timeStore)
+    #     urlContents = urllib2.urlopen('https://api.github.com/repos/felina/server/commits')
+    # except Exception, e:
+    #     pass
+    # else:
+    #     try:
+    #         print "Loading URL contents"
+    #         result = json.load(urlContents)
+    #     except Exception, e:
+    #         pass
+    #     else:
+    #         currHash = os.popen('git rev-parse HEAD').read()[:-1]
+    #         gitCurrHash = result[0]['sha']
+    #         if currHash is not gitCurrHash:
+    #             # new pull stuff
+    #             print 'New commits! ' + gitCurrHash + ' vs curr ' + currHash
+    #             #os.system('git stash && git pull')
+    #             pass
                 
 #timeStore = strftime("%Y-%m-%dT%H:%M:%S")
     sleep(60)
