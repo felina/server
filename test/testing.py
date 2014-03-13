e62af2a52ccf559ea04e65b3947c3678f28dbe2f
sambodanis <sam.bodanis@gmail.com>
2014-03-13 22:29:21 +0000

    Automated server start and mysql database reset

--- test/testing.py
+++ test/testing.py
@@ -4,20 +4,31 @@ import requests
 import json
 import os
 import re
+import atexit
+import subprocess
+import time
 
 
 port = 5000
 path = 'http://localhost:' + str(port)
 login_path = '/login'
 user_login_data = {
-	'email': 'sam.bodanis@gmail.com',
-	'pass': 'assword'
+    'email': 'sam.bodanis@gmail.com',
+    'pass': 'assword'
 }
-db_settings = json.loads(open('config/db_settings.json', 'r').read())
+with open('config/db_settings.json', 'r') as db_settings_file:
+    db_settings = json.loads(db_settings_file.read())
 
+def exit_handler():
+    swapConfigs()
+
+def swapConfigs():
+    os.system('mv config/db_settings.json test/db_settings.json')
+    os.system('mv test/db_settingsTest.json config/db_settings.json')
+    os.system('mv test/db_settings.json test/db_settingsTest.json')
 
 def exec_sql_file(cursor, sql_file):
-    print "\n[INFO] Executing SQL script file: '%s'" % (sql_file)
+    print "[INFO] Executing SQL script file: '%s'" % (sql_file)
     statement = ""
 
     for line in open(sql_file):
@@ -36,59 +47,73 @@ def exec_sql_file(cursor, sql_file):
             statement = ""
 
 def clear_database():
-	try:
-	    # os
-	    con = mdb.connect(db_settings['host'], 
-	    				  db_settings['user'], 
-	    				  db_settings['password'], 
-	    				  local_infile = 1)
-	    cur = con.cursor()
-	    cur.execute("SELECT VERSION()")
-	    ver = cur.fetchone()
-	    print "Database version : %s " % ver
-	    # cur.execute("SET sql_notes = 0;")
-	    cur.execute("DROP DATABASE IF EXISTS felina;")
-	    exec_sql_file(cur, '../tools/createDB.sql')
-	    cur.execute("USE felina;")
+    try:
+        # os
+        con = mdb.connect(db_settings['host'], 
+                          db_settings['user'], 
+                          db_settings['password'], 
+                          local_infile = 1)
+        cur = con.cursor()
+        cur.execute("SELECT VERSION()")
+        ver = cur.fetchone()
+        print "Database version : %s " % ver
+        cur.execute("SET sql_notes = 0;")
+        cur.execute("DROP DATABASE IF EXISTS felinaTest;")
+        exec_sql_file(cur, 'test/createTestDB.sql')
+        cur.execute("USE felinaTest;")
 
-	except mdb.Error, e:
-	    print "Error %d: %s" % (e.args[0],e.args[1])
-	    sys.exit(1)
-	    
-	finally:       
-	    if con:    
-	        con.close()
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        sys.exit(1)
+        
+    finally:       
+        if con:    
+            con.close()
 
-# def start_server():
-# 	server_start = os.system("nodemon src/index.js &")#.read()
-# 	print server_start
-# 	if 'Listening on ' + port not in server_start:
-# 		print 'Server did not start'
-# 		os.exit(1)
+def start_server():
+    start = time.time()
+    process = subprocess.Popen('node src/index.js', stdout=subprocess.PIPE, shell=True)
+    output = ''
+    for c in iter(lambda: process.stdout.read(1), ''):
+        if time.time() - start > 10:
+            print 'Server connection timed out'
+            sys.exit(1)
+        sys.stdout.write(c)
+        output += c
+        if 'Listening on ' + str(port) in output:
+            print '\nServer initialized'
+            return process
+
+def serverUp():
+    print 'Test 1: Server up'
+    r = requests.get(url=path)
+    expected_status_code = 200
+    if r.status_code != expected_status_code:
+        print 'The server does not appear to be up'
+        print 'Expected ' + expected_status_code + ' but got ' + r.status_code
+        os.exit(1)
+    result_object = json.loads(r.text)
+    if not result_object['res']:
+        print 'Result object is malformed: ' + json.dumps(result_object)
+        os.exit(1)
+
+def nonExistingUser():
+    print 'Test 2: Non existing user'
+    r1 = requests.post(url=path + login_path, params={'email' : 'fakeEmail@gmail.com', 'pass' : 'fakepass'})
+    if r1.text != 'Unregistered user.':
+        print 'Fake user apparently exists: ' + r1.text
 
 def main():
-	# start_server()
-	clear_database()
-	
-	print 'Testing server at path ' + path
+    swapConfigs()
+    clear_database()
+    server_process = start_server()
 
-	print 'Test 1: Server up'
-	r = requests.get(url=path)
-	expected_status_code = 200
-	if r.status_code != expected_status_code:
-		print 'The server does not appear to be up'
-		print 'Expected ' + expected_status_code + ' but got ' + r.status_code
-		os.exit(1)
-	result_object = json.loads(r.text)
-	if not result_object['res']:
-		print 'Result object is malformed: ' + json.dumps(result_object)
-		os.exit(1)
-
-	print 'Test 2: Non existing user'
-	r1 = requests.post(url=path + login_path, params={'email' : 'fakeEmail@gmail.com', 'pass' : 'fakepass'})
-	if r1.text != 'Unregistered user.':
-		print 'Fake user apparently exists: ' + r1.text
+    print 'Testing server at path ' + path
+    serverUp()
+    nonExistingUser()
+    server_process.kill()
 
 if __name__ == '__main__':
-	main()
+    atexit.register(exit_handler)
+    main()
 
