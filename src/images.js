cd8ec12f53aaf88f23929506212ad1f41acb8fe8
George Field <gf12815@my.bristol.ac.uk>
2014-03-14 16:59:53 -0400

    Overhaul image upload endpoint. Restrict accepted MIME types and make sure they are set on S3. Do not allow upload of images with conflicting hashes. Process uploaded images in parallel. (#4)

--- src/images.js
+++ src/images.js
@@ -1,16 +1,27 @@
 var fs = require('fs');
 var md5 = require('MD5');
 var aws = require('aws-sdk');
+var async = require('async');
 var errors = require('./error.js');
-//
+
 aws.config.loadFromPath('./config/aws.json');
 var s3 = new aws.S3();
 
 var PRIVATE_BUCKET = 'citizen.science.image.storage';
 var PUBLIC_BUCKET  = 'citizen.science.image.storage.public';
+
 //var S3_URL = 'https://' + PUBLIC_BUCKET + '.s3.amazonaws.com/'; // Raw bucket URL
 var S3_URL = 'http://' + PUBLIC_BUCKET + '.s3-website-eu-west-1.amazonaws.com/'; // S3 web server URL (preferred)
+
 var PRIVATE_EXPIRY = 120; // Number of seconds to keep a private image URL valid for
+
+// Defines the MIME types we accept as image uploads.
+var VALID_MIME_TYPES = [
+    'image/jpeg',
+    'image/png',
+    'image/bmp',
+    'image/tiff'
+];
 
 function fileType(filePath) {
     for (var i = filePath.length; i > 0; i--) {
@@ -78,6 +89,7 @@ function setAccess(id, priv, callback) {
     });
 }
 
+
 function imageRoutes(app, auth, db) {
     // Endpoint to get list of images
     app.get('/images', auth.enforceLogin, function(req, res) {
@@ -120,65 +132,85 @@ function imageRoutes(app, auth, db) {
         });
     });
 
+    function uploadImage(user, iInfo, callback) {
+        console.log('Trying to upload: ' + iInfo.felinaHash);
+        var params = {
+            'Bucket': PRIVATE_BUCKET,
+            'Key': iInfo.felinaHash,
+            'ContentType': iInfo.type,
+            'Body': iInfo.fileContents
+        };
+
+        return s3.putObject(params, function(err, data) {
+            if (err) {
+                console.log(err);
+                return callback(err);
+            } else {
+                // TODO: Get a project in the request somehow.
+                var pInfo = {
+                    'id': 1,
+                    'name': 'dummy'
+                };
+
+                return db.addNewImage(user, pInfo, iInfo.felinaHash, function(dbErr) {
+                    if (dbErr) {
+                        console.log(dbErr);
+                        return callback(dbErr);
+                    } else {
+                        return callback(null);
+                    }
+                });
+            }
+        });
+    }
+
     // Image/s upload endpoint
     // Uses express.multipart - this is deprecated and bad! TODO: Replace me!
     app.post('/upload/img', auth.enforceLogin, function(req, res) {
-        var resultObject = {};
-        resultObject.status = {};
+        async.each(Object.keys(req.files),
+                   function(fKey, done) {
+                       var iInfo = req.files[fKey];
 
-        var idData = req.files;
-        var images = [];
-        for (var imageName in idData) {
-            images.push(idData[imageName]);
-        }
-        if (images.length > 0) {
-            // resultObject.status.code = 0;
-            resultObject.status.code = 0;
-            resultObject.status.message = images.length.toString().concat(" images uploaded successfully");
-            resultObject.ids = [];
-            for (var i = 0; i < images.length; i++) {
-                var imageFilePath = images[i].path;
-                var fileContents = fs.readFileSync(imageFilePath); // semi sketchy decoding
-                var elementsToHash = "";
-                for (var j = 0; j < fileContents.length; j += fileContents.length / 100) {
-                    elementsToHash += fileContents[Math.floor(j)];
-                }
-                // console.log(elementsToHash);
-                var imageHash = md5(elementsToHash);
-                resultObject.ids.push(imageHash);
-                // if element hash not in database then upload to s3
-                var imageObject = {
-                    "imageData": fileContents,
-                    "imageType": fileType(imageFilePath),
-                    "imageHash": imageHash
-                };
-                uploadImage(req.user, imageObject);
-            }
-        } else {
-            resultObject.status.code = 1;
-            resultObject.status.message = "No images uploaded";
-        }
-        return res.send(resultObject);
-    });
+                       // Attempt to hash the file. If any file has an unwanted type, abort the request.
+                       if (VALID_MIME_TYPES.indexOf(iInfo.type) < 0) {
+                           // Invalid mime type, reject request.
+                           return done('Invalid file type: ' + iInfo.type + " name: " + iInfo.name);
+                       }
 
-    function uploadImage(user, imageObject) {
-        var params = {};
-        params.Bucket = 'citizen.science.image.storage';
-        params.Body = imageObject.imageData;
-        params.Key = imageObject.imageHash;
+                       // TODO: Can we avoid loading everything into memory?
+                       iInfo.fileContents = fs.readFileSync(iInfo.path); // semi sketchy decoding
+                       console.log('LENGTHS:' + iInfo.fileContents.length + ' - ' + iInfo.size);
+                       var elementsToHash = "";
+                       for (var j = 0; j < iInfo.fileContents.length; j += iInfo.fileContents.length / 100) {
+                           elementsToHash += iInfo.fileContents[Math.floor(j)];
+                       }
+                       iInfo.felinaHash = md5(elementsToHash);
 
-        s3.putObject(params, function(err, data) {
-            if (err) {
-                console.log("uploadImage error: " + err);
-            } else {
-                db.addNewImage(user, {
-                    'id': 1,
-                    'name': 'dummy'
-                }, imageObject);
-            }
-            console.log(data);
-        });
-    }
+                       return db.imageExists(iInfo.felinaHash, function(iErr, exists) {
+                           if (exists === 0) {
+                               // New image, upload!
+                               return uploadImage(req.user, iInfo, done); // Will call done() for us
+                           } else {
+                               // Existing image, reject the request.
+                               return done('Image already exists: ' + iInfo.name);
+                           }
+                       });
+                   },
+                   function(err) {
+                       // If anything errored, abort.
+                       if (err) {
+                           console.log(err);
+                           // TODO: be more clear if any images were uploaded or not.
+                           return res.send(new errors.APIErrResp(2, err));
+                       } else {
+                           // All images should have uploaded succesfully.
+                           return res.send({
+                               'res': true,
+                               'uploaded': Object.keys(req.files).length
+                           });
+                       }
+                   });
+    }); // End image upload endpoint.
 }
 
 module.exports = {
