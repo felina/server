a3ea3bc76517937571c43ebdd6ea1024aa17043b
George Field <gf12815@my.bristol.ac.uk>
2014-04-16 17:39:19 -0400

    Document errors. (#27)

--- src/error.js
+++ src/error.js
@@ -1,12 +1,27 @@
-// Convenience function to create an error to send to the client.
+/**
+ * Represents an error as represented in the API.
+ * @constructor
+ * @param {number} code - An integer representing the error.
+ * @param {string} msg - An error message to provide further detail to the client.
+ */
 function APIError(code, msg) {
     this.code = code;
     this.msg = msg;
 }
 
+/**
+ * Represents an API response in an error state. Should be sent as-is to the client as JSON.
+ * @constructor
+ * @param {number} code - An integer representing the error.
+ * @param {string} msg - An error message to provide further detail to the client.
+ */
 function APIErrResp(code, msg) {
     this.res = false;
     this.err = new APIError(code, msg);
 }
 
-module.exports = {APIErrResp:APIErrResp, APIError:APIError};
+// Export public members.
+module.exports = {
+    APIErrResp:APIErrResp,
+    APIError:APIError
+};
