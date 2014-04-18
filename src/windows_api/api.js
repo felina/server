215a16c636ea26a5460d1535c50c68cd8505f10d
George Field <gf12815@my.bristol.ac.uk>
2014-04-18 12:12:58 -0400

    Document the job server api. (#27)

--- src/windows_api/api.js
+++ src/windows_api/api.js
@@ -1,10 +1,26 @@
+/**
+ * @module jobAPI
+ */
+
 var dns = require('dns');
 var http = require('http');
 var _ = require('underscore');
+
+/**
+ * The configuration defining the connection parameters of the job server.
+ */
 var target_config = require('../../config/job_server.json');
 
+/**
+ * The timeout to use when requesting information from the job server.
+ */
 var SOCKET_TIMEOUT_MILLIS = 30000;
 
+/**
+ * Runs a test to check the job server configuration.
+ * @static
+ * @param {errorCallback} callback - The callback that handles the test outcome.
+ */
 function init(callback) {
     // Check that the config has all required values set.
     var port = parseInt(target_config.port);
@@ -29,7 +45,19 @@ function init(callback) {
     }
 }
 
-// callback(err, res)
+/**
+ * Generic job server response callback.
+ * @callback jobServerResponseCallback
+ * @param {Error|string} err - The error that caused the request to fail.
+ * @param {object} res - The object returned by the server, parsed from JSON.
+ */
+
+/**
+ * Sends a HTTP POST request to the job server.
+ * @param {string} path - The path of the resource to request.
+ * @param {object} data - The data to JSON encode and submit in the body of the request.
+ * @param {jobServerResponseCallback} callback - The callback that handles the request outcome.
+ */
 function jsPOST(path, data, callback) {
     var post_data = JSON.stringify(data);
     var post_options = {
@@ -85,7 +113,11 @@ function jsPOST(path, data, callback) {
     post_req.end();
 }
 
-// callback(err, res)
+/**
+ * Sends a HTTP GET request to the job server.
+ * @param {string} path - The path of the resource to request, including any URL parameters.
+ * @param {jobServerResponseCallback} callback - The callback that handles the request outcome.
+ */
 function jsGET(path, callback) {
     var get_options = {
         'host': target_config.host,
@@ -131,6 +163,21 @@ function jsGET(path, callback) {
     });
 }
 
+/**
+ * @typedef WorkUnit
+ * @type {object}
+ * @property {string} a - The id of the first image in the pair to process.
+ * @property {string} b - The id of the second image in the pair to process.
+ */
+
+/**
+ * Creates a job on the job server.
+ * @static
+ * @param {number} jobid - The id of the job.
+ * @param {string} zipid - The id of the zip to use for this job.
+ * @param {WorkUnit[]} pairsList - The list of image pairs to compare/process in the job.
+ * @param {jobServerResponseCallback} callback - The callback that handles the server's response.
+ */
 function createJob(jobid, zipid, pairsList, callback) {
     return jsPOST('/api/JobCreate',
                   {
@@ -148,6 +195,12 @@ function createJob(jobid, zipid, pairsList, callback) {
                   });
 }
 
+/**
+ * Fetches a job's progress from the job server.
+ * @static
+ * @param {number} jobid - The id of the job to lookup.
+ * @param {jobServerResponseCallback} callback - The callback that handles the server's response.
+ */
 function getProgress(jobid, callback) {
     return jsGET('/api/JobProgress?jobid=' + encodeURIComponent(jobid), function(err, res) {
         if (err) {
@@ -159,6 +212,12 @@ function getProgress(jobid, callback) {
     });
 }
 
+/**
+ * Fetches a job's results from the job server.
+ * @static
+ * @param {number} jobid - The id of the job to lookup.
+ * @param {jobServerResponseCallback} callback - The callback that handles the results of the job.
+ */
 function getResults(jobid, callback) {
     return jsGET('/api/JobResults?jobid=' + encodeURIComponent(jobid), function(err, res) {
         if (err) {
@@ -170,9 +229,26 @@ function getResults(jobid, callback) {
     });
 }
 
+/**
+ * Supported actions when controlling a job.
+ *
+ * PAUSE   : The job should be paused, to resume execution in the future.
+ * STOP    : The job should be cancelled, and should not be able to be resumed.
+ * RESUME  : The job should be resumed from the point where it was paused.
+ * RESTART : The job should be restarted from the beginning, discarding it's results
+ * @static
+ */
 var JOB_CONTROL_ACTIONS = ['PAUSE', 'STOP', 'RESUME', 'RESTART'];
 
 // If terminate, the job should be cancelled and will not be possible to be resumed.
+
+/**
+ * Sends a job control action to the job server.
+ * @static
+ * @param {number} jobid - The id of the job to control.
+ * @param {string} action - The action to attempt on the job.
+ * @param {jobServerResponseCallback} callback - The callback that handles the server's response.
+ */
 function controlJob(jobid, action, callback) {
     if (JOB_CONTROL_ACTIONS.indexOf(action) < 0) {
         return callback('Invalid action.');
@@ -193,6 +269,11 @@ function controlJob(jobid, action, callback) {
     }
 }
 
+/**
+ * Fetches the queue of jobs from the job server.
+ * @static
+ * @param {jobServerResponseCallback} callback - The callback that handles the server's job queue.
+ */
 function getQueue(callback) {
     return jsGET('/api/JobQueue', function(err, res) {
         if (err) {
@@ -204,6 +285,7 @@ function getQueue(callback) {
     });
 }
 
+// Export all public members.
 module.exports = {
     init: init,
     createJob: createJob,
