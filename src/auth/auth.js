39707af0cc2984f9c2d7ebbaf556e5c94da73c2c
George Field <gf12815@my.bristol.ac.uk>
2014-04-18 13:04:08 -0400

    Document auth. (#27)

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,3 +1,7 @@
+/**
+ * @module auth
+ */
+
 var _ = require('underscore');
 var loauth = require('./localauth.js');
 var extauth = require('./externalauth.js');
@@ -7,6 +11,10 @@ var errors = require('../error.js');
 
 // Middleware to enforce login.
 // stackoverflow.com/questions/18739725/
+/**
+ * Express middleware to only allow access to the resource to a logged in user.
+ * @static
+ */
 function enforceLogin(req, res, next) {
     // user will be set if logged in
     if (req.user) {
@@ -17,6 +25,27 @@ function enforceLogin(req, res, next) {
     }
 }
 
+/**
+ * @typedef userVerifier
+ * @type {function}
+ * @param {user.User} user - The user trying to request the resource.
+ * @returns {boolean} The trut value indicating if the request should be allowed to continue or not.
+ */
+
+/**
+ * @typedef EnforceLoginOptions
+ * @type {object}
+ * @property {string[]} [ips] - A list of ips to restrict access to.
+ * @property {number} [minPL] - The minimum privilege leve/usertype to allow access to.
+ * @property {userVerifier} [verifier] - A function that decides if the request should continue, given the user object.
+ */
+
+/**
+ * Function to create Express middleware based upon an options object, that will enforce various access restrictions.
+ * @static
+ * @param {EnforceLoginOptions} options - The options that specify the restrictions this middleware will enforce.
+ * @returns {functions} The Express middlewares to add to the route definition.
+ */
 function enforceLoginCustom(options, req, res, next) {
     if (options === null || typeof options === 'undefined') {
         // options hasn't been supplied, fall back to standard behaviour
@@ -28,6 +57,7 @@ function enforceLoginCustom(options, req, res, next) {
         return enforceLogin(req, res, next);
     }
 
+    // The other middlewares we might use expect the user to be logged in.
     var middlewares = [ enforceLogin ];
 
     if (_.isArray(options.ips)) {
@@ -76,6 +106,11 @@ function enforceLoginCustom(options, req, res, next) {
     return middlewares;
 }
 
+/**
+ * Function to setup Passport for use throughout the application.
+ * @static
+ * @param {object} passport - The main Passport object.
+ */
 function authSetup(passport) {
     passport.serializeUser(function(user, done) {
         // Stores the user's id into session so we can retrieve their info on next load.
@@ -94,13 +129,23 @@ function authSetup(passport) {
     passport.use(extauth.fbStrategy);
 }
 
+/**
+ * Registers Express routes related to authorization actions. These are API endpoints.
+ * @static
+ * @param {Express} app - The Express application object.
+ */
 function authRoutes(app) {
-    // Import the facebook routes
+    // Import the facebook auth routes
     extauth.fbRoutes(app);
 
     // Import the local auth routes
     loauth.authRoutes(app, enforceLoginCustom);
 
+    /**
+     * API endpoint to logout the current user.
+     * @hbcsapi {GET} logout
+     * @returns {BasicAPIResponse} The API response detailing the outcome of the logout operation.
+     */
     app.get('/logout', function(req, res) {
         if (req.user) {
             req.logout();
@@ -112,12 +157,29 @@ function authRoutes(app) {
         }
     });
 
-    // Checks if user is logged in, returns the user object.
+    /**
+     * @typedef UserAPIResponse
+     * @type {object}
+     * @property {boolean} res - True iff the user is logged in.
+     * @property {APIError} [err] - The error that occured trying to verify the user's login state.
+     * @property {user.User} [user] - The user object of the current user.
+     */
+
+    /**
+     * API endpoint to check that the requester is logged in, and to retrieve their user info.
+     * @hbcsapi {GET} logincheck
+     * @returns {UserAPIResponse} The API response providing the currently logged in user's information.
+     */
     app.get('/logincheck', enforceLogin, function(req, res) {
-        res.send({'res':true, 'user':req.user});
+        // Response to not logged in users will be provided by the middleware.
+        res.send({
+            'res':true
+            'user':req.user
+        });
     });
 }
 
+// Export all public members.
 module.exports = {
     authSetup:authSetup,
     authRoutes:authRoutes,
