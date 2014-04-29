f2b8a5433417bb8e3574a78163bc89eb8edd0d42
George Field <gf12815@my.bristol.ac.uk>
2014-04-29 13:07:30 -0400

    CmdString? What's that?

--- src/models/Job.js
+++ src/models/Job.js
@@ -5,17 +5,6 @@
 var _ = require('underscore');
 
 /**
- * A format string detailing how to invoke the executable within the zip for a given job.
- * The string should represent the command line that would run the executable file in the current directory.
- * Any variable parameters are delimited by '%' characters, e.g. '%imageA%'.
- * Allowed parameters are:
- *   - imageA = The path to the 'a' image.
- *   - imageB = The path to the 'b' image.
- * @typedef CmdString
- * @type {string}
- */
-
-/**
  * @typedef WorkImage
  * @type {object}
  * @property {string} bucket - The bucket in which the image can be found.
@@ -35,7 +24,7 @@ var _ = require('underscore');
  * @alias module:Job
  * @param {number} id - The unique id of the job.
  * @param {number} zip - The unique id of the zip that holds the executables required for this job.
- * @param {CmdString} cmd - The command line format string that gives the command needed to run the job.
+ * @param {string} cmd - The name of the executable file within the zip to run.
  * @param {WorkUnit[]} work - The image pairs the job should be performed on.
  */
 function Job(id, zip, cmd, work) {
