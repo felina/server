612201060193545fe968087ab1263ce24daf32fa
sambodanis <sam.bodanis@gmail.com>
2014-05-05 15:23:05 +0100

    random jobs now have names

--- src/jobs.js
+++ src/jobs.js
@@ -366,6 +366,7 @@ function getJobs(req, res) {
                         comp = false;
                     }
                     done.push({
+                        name: 'Job ' + i,
                         jobid:i,
                         started:true,
                         completed:comp,
